8d6104b2125d4b968be0fc734c3d6eb14da37873
tzikoulisathan <tzikoulisathan@gmail.com>
2023-11-14 17:07:48 +0200

    Fix date saving and invoice retrieving

--- VATTracker/Utilities/DateToString.swift
+++ VATTracker/Utilities/DateToString.swift
@@ -6,12 +6,3 @@
 //
 
 import Foundation
-
-func dateToString(inputDate: Date) -> String {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    
-    let date = formatter.string(from: inputDate)
-    
-    return date
-}
