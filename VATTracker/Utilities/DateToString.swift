e9d87547016a1d194cb0c912fc0af0aaec3a3f1e
tzikoulisathan <tzikoulisathan@gmail.com>
2023-11-14 11:20:10 +0200

    Bug fixing

---
+++ VATTracker/Utilities/DateToString.swift
@@ -0,0 +1,17 @@
+//
+//  DateToString.swift
+//  VATTracker
+//
+//  Created by Athanasios Tzikoulis on 14/11/23.
+//
+
+import Foundation
+
+func dateToString(inputDate: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    
+    let date = formatter.string(from: inputDate)
+    
+    return date
+}
