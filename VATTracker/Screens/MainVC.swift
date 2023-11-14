8bc2984df2fa741d889092c07da490c0d6d6ac33
tzikoulisathan <tzikoulisathan@gmail.com>
2023-11-14 19:50:25 +0200

    Initial functioning of the report screen

--- VATTracker/Screens/MainVC.swift
+++ VATTracker/Screens/MainVC.swift
@@ -17,15 +17,25 @@ class MainVC: UIViewController {
     
     //MARK: - UI Elements
     let titleView = OverviewTitleVC()
-    let stackView = UIStackView()
+    let vatLabel: UILabel = {
+       let label = UILabel()
+        label.text = "VAT Balance"
+        return label
+    }()
+    let vatBalanceLabel = UILabel()
+    let cashLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Cash Balance"
+        return label
+    }()
+    let cashBalanceLabel = UILabel()
     
 
+    //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBackground
         
-        addTitleView()
+        setupUI()
         
     }
     
@@ -34,9 +44,9 @@ class MainVC: UIViewController {
         
         loadInvoices()
         let x = getTotalAmount()
-        print(x)
+        vatBalanceLabel.text = String(format: "%2f", x)
         let y = getTotalVatAmount()
-        print(y)
+        cashBalanceLabel.text = String(format: "%2f", y)
     }
     
     //MARK: - Functions
@@ -53,6 +63,8 @@ class MainVC: UIViewController {
     
     
     //MARK: - Data manipulation functions
+    
+    //TODO: make functions take date range as a parameter and calculate total amount and vat amount for those datesw
     private func getTotalAmount() -> Double {
         
         var totalAmount: Double = 0
@@ -86,21 +98,52 @@ class MainVC: UIViewController {
     }
     
     //MARK: - UI Methods
-    func addTitleView() {
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        addTitleView()
+
+    }
+    
+    
+    private func addTitleView() {
         addChild(titleView)
         view.addSubview(titleView.view)
         titleView.didMove(toParent: self)
-        setTitleViewConstraints()
+        setConstraints()
     }
     
-    func setTitleViewConstraints() {
+    private func setConstraints() {
+        let views  = [vatLabel, vatBalanceLabel, cashLabel, cashBalanceLabel]
+        
+        for everyView in views {
+            view.addSubview(everyView)
+            everyView.translatesAutoresizingMaskIntoConstraints = false
+        }
         titleView.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 12
         
         NSLayoutConstraint.activate([
             titleView.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             titleView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             titleView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            titleView.view.heightAnchor.constraint(equalToConstant: 150)
+            titleView.view.heightAnchor.constraint(equalToConstant: 150),
+            
+            vatLabel.topAnchor.constraint(equalTo: titleView.view.bottomAnchor, constant: padding),
+            vatLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vatLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            vatBalanceLabel.topAnchor.constraint(equalTo: vatLabel.bottomAnchor, constant: padding),
+            vatBalanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vatBalanceLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            cashLabel.topAnchor.constraint(equalTo: vatBalanceLabel.bottomAnchor, constant: padding),
+            cashLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cashLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            cashBalanceLabel.topAnchor.constraint(equalTo: cashLabel.bottomAnchor, constant: padding),
+            cashBalanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cashBalanceLabel.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
     
