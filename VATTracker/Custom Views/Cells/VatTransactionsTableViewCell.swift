63272b83025674504e8743453ed97b11a88bcda4
tzikoulisathan <tzikoulisathan@gmail.com>
2023-11-28 12:38:22 +0200

    VAT transaction list implementation

---
+++ VATTracker/Custom Views/Cells/VatTransactionsTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  VTVatTransactionsTableViewCell.swift
+//  VATTracker
+//
+//  Created by Athanasios Tzikoulis on 28/11/23.
+//
+
+import UIKit
+
+class VatTransactionsTableViewCell: UITableViewCell {
+    
+    //MARK: - Variables
+    
+    
+    //MARK: - UI Components
+    
+    
+    //MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    //MARK: - UI Setup
+    
+    
+    //MARK: - Selectors
+
+    
+    static let identifier = "VTVatTransactionsTableViewCell"
+
+ 
+
+}
