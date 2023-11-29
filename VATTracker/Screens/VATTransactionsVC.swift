b9af57134f7482932e91d4c9ada9b8be5d659b71
tzikoulisathan <tzikoulisathan@gmail.com>
2023-11-29 10:56:58 +0200

    Commit minor changes in order to merge to main

--- VATTracker/Screens/VATTransactionsVC.swift
+++ VATTracker/Screens/VATTransactionsVC.swift
@@ -62,7 +62,7 @@ class VATTransactionsVC: UIViewController {
 
 extension VATTransactionsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return paymentsList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
